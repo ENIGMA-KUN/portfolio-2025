0fe2288dd703b4f61d1a9356f4a077c601d97a39
enigma-kun <csking007kog@gmail.com>
2025-04-05 21:00:12 -0700

    main

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,36 +1,13 @@
-import type { NextPage } from 'next';
-import Layout from '../components/layout/Layout';
-import BackgroundAnimation from '../components/three/BackgroundAnimation';
-import HeroSection from '../components/sections/HeroSection';
-import AboutSection from '../components/sections/AboutSection';
-import ExperienceSection from '../components/sections/ExperienceSection';
-import ProjectsSection from '../components/sections/ProjectSection';
-import ResearchSection from '../components/sections/ResearchSection';
-import ContactSection from '../components/sections/ContactSection';
+import { Html, Head, Main, NextScript } from 'next/document'
 
-const Home: NextPage = () => {
+export default function Document() {
   return (
-    <Layout>
-      {/* Background Particles */}
-      <BackgroundAnimation />
-      
-      {/* Hero Section */}
-      <HeroSection />
-      
-      {/* Main Content */}
-      <div className="bg-white dark:bg-dark">
-        <AboutSection />
-        <div className="bg-gray-50 dark:bg-gray-900">
-          <ExperienceSection />
-        </div>
-        <ProjectsSection />
-        <div className="bg-gray-50 dark:bg-gray-900">
-          <ResearchSection />
-        </div>
-        <ContactSection />
-      </div>
-    </Layout>
-  );
-};
-
-export default Home;
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
